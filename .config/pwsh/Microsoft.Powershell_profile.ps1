153d1d393a3fc97813d198d5f5505562b4cfbb00
Steve Warren <steve-warren@users.noreply.github.com>
2022-10-20 15:54:55 -0500

    added pwsh profile.

---
+++ .config/pwsh/Microsoft.Powershell_profile.ps1
@@ -0,0 +1,4 @@
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/steve-warren/dotfiles/master/omp/warren.omp.json' | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+Set-Alias g git
