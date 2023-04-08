c677ee0a75d2c8121e9d32a41da3a78588b5b178
Steve Warren <steve-warren@users.noreply.github.com>
2023-04-08 00:32:56 -0500

    added additional dotnet aliases.

--- .config/pwsh/Microsoft.Powershell_profile.ps1
+++ .config/pwsh/Microsoft.Powershell_profile.ps1
@@ -1,5 +1,12 @@
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/steve-warren/dotfiles/master/omp/warren.omp.json' | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
+
 Set-Alias g git
 Set-Alias d dotnet
+
+Function DotnetRun { dotnet run }
+Set-Alias dr DotnetRun
+
+Function DotnetTest { dotnet test }
+Set-Alias dt DotnetTest
