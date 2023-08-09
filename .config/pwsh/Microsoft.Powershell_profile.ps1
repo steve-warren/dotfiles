cb406c68118563264332ce502614f954b3607cab
Steve Warren <steve-warren@users.noreply.github.com>
2023-08-08 20:12:43 -0500

    set powershell profile omp to local path

--- .config/pwsh/Microsoft.Powershell_profile.ps1
+++ .config/pwsh/Microsoft.Powershell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/steve-warren/dotfiles/master/omp/warren.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config $env:USERPROFILE/.dotfiles/omp/warren.omp.json | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
